7088f28674a3745184ab638b5250739d4fec1998
subhendu17620 <subhendu17620@gmail.com>
2020-10-05 00:09:03 +0530

    added webpushr

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React from "react";
 import poster from "./poster.svg";
 import "./App.css";
-import OneSignal from "react-onesignal";
+// import OneSignal from "react-onesignal";
 
 function App() {
-  const options = {
-    allowLocalhostAsSecureOrigin: true,
-    requiresUserPrivacyConsent: true,
-    persistNotification: true,
-    autoResubscribe: true,
-    autoRegister: true,
-    notificationClickHandlerMatch: "string",
-    notificationClickHandlerAction: "string",
-    notifyButton: {
-      enable: true,
-      size: "large",
-      position: "bottom-right",
-      showCredit: true,
-      prenotify: true,
-      theme: "default",
-      offset: {
-        bottom: "hi",
-        right: "hello",
-        left: "hello",
-      },
-    },
-  };
-  OneSignal.initialize("2fb754f3-e6ce-4ba7-a89e-f16aa62ad3b9", options);
   return (
     <div className="App">
       <header className="App-header">
